c3fe28726a117d030f544c8cf438b54024be6fc7
mdnurakmal <mdnurakmal@gmail.com>
2021-09-14 20:16:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,12 +8,11 @@ then
     echo "bucket exist"
     terraform state pull
     terraform destroy
-    
+
 else
     gsutil mb gs://cloud-resume-challenge-bucket
 fi
 
 cd terraform
 terraform init
-terraform state pull
 terraform apply -auto-approve -var region=$1
