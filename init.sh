7bcfbc27fca515a5171d1bcec4b23d290fc9586f
mdnurakmal <mdnurakmal@gmail.com>
2021-09-14 19:52:58 +0800

    update

--- init.sh
+++ init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-gcloud auth activate-service-account "github@$1.iam.gserviceaccount.com" --key-file=$3 --project=$1
-gcloud config set run/region $2
-
 gsutil -q stat gs://cloud-resume-challenge-bucket/terraform/state/default.tfstate
 
 return_value=$?
@@ -16,4 +12,4 @@ fi
 
 cd terraform && terraform init
 cd terraform && terraform state pull
-cd terraform && terraform apply -auto-approve -var region=$2
+cd terraform && terraform apply -auto-approve -var region=$1
