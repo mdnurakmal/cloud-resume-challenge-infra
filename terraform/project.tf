cda20d2c77d859e4c68004628be75a753bcf8876
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 16:12:41 +0800

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -34,7 +34,7 @@ resource "google_app_engine_application" "app" {
   database_type = "CLOUD_FIRESTORE"
   location_id = var.region
 
-  depends_on = [google_project_service.appengine]
+  depends_on = [google_project_service.appengine,google_project_service.firestore]
 }
 
 
