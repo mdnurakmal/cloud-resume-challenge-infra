7e35ab8e0e4cc2770ebd122720f4643e7d72c18f
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 02:07:16 +0800

    update

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,5 +4,12 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
   display_name = "Cloud Resume Challenge Worker"
 }
 
+# cloud function SA is running cloud function as a service account which requires serviceaccountuser permission
+resource "google_project_iam_binding" "serviceAccountUser_permmisions" {
 
+  role = "roles/iam.serviceAccountUser"
+
+  members = [local.cloud_resume_challenge_worker_sa]
+  depends_on = [google_service_account.cloud_resume_challenge_worker]
+}
 
