20915e77a1027a9252bbb486a3474fdd5a2dcdfc
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 16:37:41 +0800

    Update build_cloudfunction.tf

--- terraform/build_cloudfunction.tf
+++ terraform/build_cloudfunction.tf
@@ -14,3 +14,25 @@ resource "google_storage_bucket_object" "archive" {
   source = "${var.path}/http_trigger.zip"
   depends_on = [data.archive_file.http_trigger]
 }
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs14"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
