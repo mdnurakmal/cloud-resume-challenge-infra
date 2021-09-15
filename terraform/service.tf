31dfe4a3d842683539397180cdccf493669f8ec4
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 01:58:51 +0800

    Update service.tf

--- terraform/service.tf
+++ terraform/service.tf
@@ -29,8 +29,8 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.resume.location
-  project  = google_cloud_run_service.resume.project
-  service  = google_cloud_run_service.resume.name
+  location = google_cloud_run_service.cloud-resume-challenge.location
+  project  = google_cloud_run_service.cloud-resume-challenge.project
+  service  = google_cloud_run_service.cloud-resume-challenge.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
