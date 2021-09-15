f2bad1bf63a74c6503412eb7adf6689366b9f15d
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 22:37:34 +0800

    Update sa.tf

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -21,7 +21,7 @@ resource "google_project_iam_binding" "serviceAccountUser_permmisions" {
 }
 
 resource "google_project_iam_binding" "cloudfunctions_permissions" {
-  role = "roles/cloudfunctions.admin"
+  role = "roles/cloudfunctions.developer"
 
   members = [local.cloud_resume_challenge_worker_sa]
   depends_on = [google_project_iam_binding.serviceAccountUser_permmisions]
