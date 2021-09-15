92f14a83ff23efa3986639fca032c54f8f7d5be6
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 11:52:12 +0800

    UPDATE

---
+++ terraform/build_cloudfunction.tf
@@ -0,0 +1,16 @@
+resource "google_storage_bucket" "bucket" {
+  name = "cloudfunction-deploy-test1"
+}
+
+data "archive_file" "http_trigger" {
+  type        = "zip"
+  output_path = "http_trigger.zip"
+  source_dir = "BE"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "http_trigger.zip"
+  bucket = "${google_storage_bucket.bucket.name}"
+  source = "http_trigger.zip"
+  depends_on = ["data.archive_file.http_trigger"]
+}
