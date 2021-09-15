10c1a3b3811c3c18e934ee59c8f8c971d2dee950
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 00:33:04 +0800

    Revert "Update sa.tf"
    This reverts commit 95426f489dcea607d8943dca8c0d1607961adb70.

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,13 +4,21 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
   display_name = "Cloud Resume Challenge Worker"
 }
 
+resource "google_service_account_iam_binding" "admin-account-iam" {
+  service_account_id = google_service_account.cloud_resume_challenge_worker.name
+  role = "roles/iam.serviceAccountUser"
+
+  members = ["serviceAccount:github@${local.project}.iam.gserviceaccount.com"]
+  depends_on = [google_service_account.cloud_resume_challenge_worker]
+}
+
 
 resource "google_project_iam_binding" "serviceAccountUser_permmisions" {
 
   role = "roles/iam.serviceAccountUser"
 
   members = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_service_account.cloud_resume_challenge_worker]
+  depends_on = [google_service_account_iam_binding.admin-account-iam]
 }
 
 resource "google_project_iam_binding" "cloudfunctions_permissions" {
