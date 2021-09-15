4cea4613d95c951acc06399d0be11e0bdaccc7d7
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 01:28:11 +0800

    update

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,33 +4,5 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
   display_name = "Cloud Resume Challenge Worker"
 }
 
-# Github SA requires permission to access cloud function SA
-# 2 seperate SA to provide finer access controls
-resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = google_service_account.cloud_resume_challenge_worker.name
-  role = "roles/iam.serviceAccountUser"
-
-  members = ["serviceAccount:github@${local.project}.iam.gserviceaccount.com"]
-  depends_on = [google_service_account.cloud_resume_challenge_worker]
-}
-
-# cloud function SA is running cloud function as a service account which requires serviceaccountuser permission
-resource "google_project_iam_binding" "serviceAccountUser_permmisions" {
-
-  role = "roles/iam.serviceAccountUser"
-
-  members = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_service_account_iam_binding.admin-account-iam]
-}
-
-# Set cloud function permission 
-resource "google_project_iam_binding" "cloudfunctions_permissions" {
-  role = "roles/cloudfunctions.developer"
-
-  members = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_project_iam_binding.serviceAccountUser_permmisions]
-}
-
-
 
 
