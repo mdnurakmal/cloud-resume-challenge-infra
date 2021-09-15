d145297427d8c1fa68e2f93ce5aca541e900e1b1
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 20:42:49 +0800

    Update sa.tf

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -6,24 +6,24 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
 
 # Set permissions
 resource "google_project_iam_binding" "serviceAccountKeyAdmin_permissions" {
-  project = local.project
+
   role       = "roles/iam.serviceAccountKeyAdmin"
   members    = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_service_account.cloud_resume_challenge_worker]
+  depends_on = [google_service_account.cloud_resume_challenge_worker,$MEMBER]
 }
 
 # Set permissions
 resource "google_project_iam_binding" "serviceAccountUser_permissions" {
-  project = local.project
+ 
   role       = "roles/iam.serviceAccountUser"
-  members    = [local.cloud_resume_challenge_worker_sa]
+  members    = [local.cloud_resume_challenge_worker_sa,$MEMBER]
   depends_on = [google_project_iam_binding.serviceAccountKeyAdmin_permissions]
 }
 
 # Set permissions
 resource "google_project_iam_binding" "cloudfunctions_permissions" {
-  project = local.project
+
   role       = "roles/cloudfunctions.admin"
-  members    = [local.cloud_resume_challenge_worker_sa]
+  members    = [local.cloud_resume_challenge_worker_sa,$MEMBER]
   depends_on = [google_project_iam_binding.serviceAccountUser_permissions]
 }
