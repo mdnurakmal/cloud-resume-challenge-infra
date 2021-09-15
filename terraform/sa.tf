0e1ed70e7935f3533172f5a4f512a74007917a65
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 21:47:26 +0800

    Update sa.tf

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,20 +4,27 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
   display_name = "Cloud Resume Challenge Worker"
 }
 
-
-resource "google_project_iam_binding" "admin-account-iam" {
-
+resource "google_service_account_iam_binding" "admin-account-iam" {
+  service_account_id = google_service_account.sa.cloud_resume_challenge_worker
   role = "roles/iam.serviceAccountUser"
 
   members = [local.cloud_resume_challenge_worker_sa]
   depends_on = [google_service_account.cloud_resume_challenge_worker]
 }
 
+resource "google_project_iam_binding" "serviceAccountUser_permmisions" {
+
+  role = "roles/iam.serviceAccountUser"
+
+  members = [local.cloud_resume_challenge_worker_sa]
+  depends_on = [google_service_account_iam_binding.admin-account-iam]
+}
+
 resource "google_project_iam_binding" "cloudfunctions_permissions" {
   role = "roles/cloudfunctions.developer"
 
   members = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_project_iam_binding.admin-account-iam]
+  depends_on = [google_project_iam_binding.serviceAccountUser_permmisions]
 }
 
 
