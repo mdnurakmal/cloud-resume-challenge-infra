5c84473a7c9ecc958ead0e28049116cef02c9ece
mdnurakmal <mdnurakmal@gmail.com>
2021-09-15 21:09:32 +0800

    Update sa.tf

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,26 +4,30 @@ resource "google_service_account" "cloud_resume_challenge_worker" {
   display_name = "Cloud Resume Challenge Worker"
 }
 
-# Set permissions
-resource "google_project_iam_binding" "serviceAccountKeyAdmin_permissions" {
+
+resource "google_service_account_iam_binding" "admin-account-iam" {
   service_account_id = google_service_account.cloud_resume_challenge_worker.name
-  role       = "roles/iam.serviceAccountKeyAdmin"
-  members    = [local.cloud_resume_challenge_worker_sa]
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [local.cloud_resume_challenge_worker_sa]
   depends_on = [google_service_account.cloud_resume_challenge_worker]
 }
 
-# Set permissions
-resource "google_project_iam_binding" "serviceAccountUser_permissions" {
+resource "google_service_account_iam_binding" "cloudfunctions_permissions" {
   service_account_id = google_service_account.cloud_resume_challenge_worker.name
-  role       = "roles/iam.serviceAccountUser"
-  members    = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_project_iam_binding.serviceAccountKeyAdmin_permissions]
+  role               = "roles/cloudfunctions.admin"
+
+  members = [local.cloud_resume_challenge_worker_sa]
+  depends_on = [google_service_account_iam_binding.admin-account-iam]
 }
 
-# Set permissions
-resource "google_project_iam_binding" "cloudfunctions_permissions" {
+
+resource "google_service_account_iam_binding" "serviceAccountKeyAdmin_permissions" {
   service_account_id = google_service_account.cloud_resume_challenge_worker.name
-  role       = "roles/cloudfunctions.admin"
-  members    = [local.cloud_resume_challenge_worker_sa]
-  depends_on = [google_project_iam_binding.serviceAccountUser_permissions]
+  role               = "roles/iam.serviceAccountKeyAdmin"
+
+  members = [local.cloud_resume_challenge_worker_sa]
+  depends_on = [google_service_account_iam_binding.cloudfunctions_permissions]
 }
+
+
