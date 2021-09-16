c61806241c560fd57dedb6417fd7a6266ec2fdea
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 15:25:02 +0800

    Update firestore.tf

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -5,3 +5,13 @@ resource "google_firestore_document" "mydoc" {
   fields      = "{\"visitorCount\":{\"mapValue\":{\"fields\":{\"akey\":{\"integerValue\":\"0\"}}}}}"
 }
 
+resource "google_firestore_document" "hasInit" {
+
+  collection  = "cloud-resume-challenge-collection"
+  document_id = "cloud-resume-challenge"
+  fields      = "{\"hasInit\":{\"mapValue\":{\"fields\":{\"akey\":{\"booleanValue\":\"False\"}}}}}"
+}
+
+
+
+
