d265756e6135ec7c1fb14bded095b4d1338d5674
mdnurakmal <mdnurakmal@gmail.com>
2021-09-22 00:01:30 +0800

    Update artifact.tf

--- terraform/artifact.tf
+++ terraform/artifact.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
-
+  project = local.project
   location = var.region
   repository_id = "cloud-resume-challenge"
   description = "cloud-resume-challenge"
