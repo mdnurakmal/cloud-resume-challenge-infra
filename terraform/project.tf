be8dce62f04dc52c65374f8b5e69c821458b1d23
mdnurakmal <mdnurakmal@gmail.com>
2021-09-22 01:47:54 +0800

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -6,22 +6,18 @@ resource "google_project_service" "iam" {
 
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
-
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
- 
 }
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
- 
 }
 
 resource "google_project_service" "firestore" {
   service = "firestore.googleapis.com"
-
 }
 
 resource "google_project_service" "appengine" {
@@ -30,50 +26,40 @@ resource "google_project_service" "appengine" {
 
 resource "google_project_service" "containerregistry" {
   service = "containerregistry.googleapis.com"
-
 }
 
 resource "google_project_service" "firebase" {
   service = "firebase.googleapis.com"
-
 }
 
 resource "google_project_service" "apigateway" {
   service = "apigateway.googleapis.com"
-
 }
 
 resource "google_project_service" "servicecontrol" {
   service = "servicecontrol.googleapis.com"
-
 }
 
 resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
-
 }
 
 resource "google_project_service" "serviceusage" {
   service = "serviceusage.googleapis.com"
-  
 }
 
 resource "google_project_service" "sourcerepo" {
   service = "sourcerepo.googleapis.com"
-
 }
 
 resource "google_project_service" "firebaserules" {
   service = "firebaserules.googleapis.com"
-
 }
 
 resource "google_project_service" "domains" {
   service = "domains.googleapis.com"
-
 }
 
 resource "google_project_service" "dns" {
   service = "dns.googleapis.com"
-
 }
