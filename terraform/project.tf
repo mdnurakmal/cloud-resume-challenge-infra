2ebc7308e6058eb38beeb6905f426fe3b642f6c3
mdnurakmal <mdnurakmal@gmail.com>
2021-09-22 01:40:28 +0800

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -2,113 +2,78 @@
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
-  disable_on_destroy = true
-
-  
 }
 
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = true
-
-  
+ 
 }
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
-  disable_on_destroy = true
-
-  
+ 
 }
 
 resource "google_project_service" "firestore" {
   service = "firestore.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "appengine" {
   service = "appengine.googleapis.com"
-  disable_on_destroy = true
-
-  
 }
 
 resource "google_project_service" "containerregistry" {
   service = "containerregistry.googleapis.com"
-  disable_on_destroy = true
-
- 
 
 }
 
 resource "google_project_service" "firebase" {
   service = "firebase.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "apigateway" {
   service = "apigateway.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "servicecontrol" {
   service = "servicecontrol.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "serviceusage" {
   service = "serviceusage.googleapis.com"
-  disable_on_destroy = true
-
   
 }
 
 resource "google_project_service" "sourcerepo" {
   service = "sourcerepo.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "firebaserules" {
   service = "firebaserules.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "domains" {
   service = "domains.googleapis.com"
-  disable_on_destroy = true
 
-  
 }
 
 resource "google_project_service" "dns" {
   service = "dns.googleapis.com"
-  disable_on_destroy = true
-
 
 }
