600a0c448968b1f45345e1797f5b8cb006970372
mdnurakmal <mdnurakmal@gmail.com>
2021-09-21 16:58:57 +0800

    update

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-gcloud auth configure-docker
-cd BE
-docker build . -t "gcr.io/${2}/cloud-resume-challenge"
-docker push "gcr.io/${2}/cloud-resume-challenge"
-dockerSHA=`gcloud container images describe "gcr.io/${2}/cloud-resume-challenge" --format 'value(image_summary.digest)'`
-echo $dockerSHA
-cd ..
+# gcloud auth configure-docker
+# cd BE
+# docker build . -t "gcr.io/${2}/cloud-resume-challenge"
+# docker push "gcr.io/${2}/cloud-resume-challenge"
+# dockerSHA=`gcloud container images describe "gcr.io/${2}/cloud-resume-challenge" --format 'value(image_summary.digest)'`
+# echo $dockerSHA
+# cd ..
 
 isServiceEnabled=`gcloud services list --enabled | grep -w -c "cloudresourcemanager.googleapis.com"`
 
@@ -40,24 +40,24 @@ fi
 terraform -chdir=terraform import google_app_engine_application.app $2
 terraform -chdir=terraform apply -auto-approve -var region=$1 -var imagesha=$dockerSHA
 
-#check if bucket exists and not empty
-gsutil -q stat gs://cloud-resume-challenge-frontend-bucket/index.html
+# #check if bucket exists and not empty
+# gsutil -q stat gs://cloud-resume-challenge-frontend-bucket/index.html
 
-return_value=$?
+# return_value=$?
 
-if [[ $return_value == 0 ]] 
-then
-    echo "deleting existing FE folder"
-    gsutil rm -r gs://cloud-resume-challenge-frontend-bucket
-fi
+# if [[ $return_value == 0 ]] 
+# then
+#     echo "deleting existing FE folder"
+#     gsutil rm -r gs://cloud-resume-challenge-frontend-bucket
+# fi
 
-gsutil mb gs://cloud-resume-challenge-frontend-bucket/
-echo "uploading FE folder"
+# gsutil mb gs://cloud-resume-challenge-frontend-bucket/
+# echo "uploading FE folder"
 
-gsutil cp -r FE/* gs://cloud-resume-challenge-frontend-bucket/
+# gsutil cp -r FE/* gs://cloud-resume-challenge-frontend-bucket/
 
-#allow public acess to the bucket
-gsutil iam ch allUsers:objectViewer gs://cloud-resume-challenge-frontend-bucket
+# #allow public acess to the bucket
+# gsutil iam ch allUsers:objectViewer gs://cloud-resume-challenge-frontend-bucket
 
-#set default static page for the bucket
-gsutil web set -m index.html -e 404.html gs://cloud-resume-challenge-frontend-bucket
+# #set default static page for the bucket
+# gsutil web set -m index.html -e 404.html gs://cloud-resume-challenge-frontend-bucket
