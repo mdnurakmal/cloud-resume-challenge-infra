8b1212f62264072a57e319740499234977b7e88c
mdnurakmal <mdnurakmal@gmail.com>
2021-09-21 22:27:28 +0800

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,32 +1,45 @@
 #enable all necessary apis
+
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "firestore" {
   service = "firestore.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "appengine" {
   service = "appengine.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "containerregistry" {
@@ -37,44 +50,62 @@ resource "google_project_service" "containerregistry" {
 resource "google_project_service" "firebase" {
   service = "firebase.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "apigateway" {
   service = "apigateway.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "servicecontrol" {
   service = "servicecontrol.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "serviceusage" {
   service = "serviceusage.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "sourcerepo" {
   service = "sourcerepo.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "firebaserules" {
   service = "firebaserules.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "domains" {
   service = "domains.googleapis.com"
   disable_on_destroy = true
+
+    depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
 
 resource "google_project_service" "dns" {
   service = "dns.googleapis.com"
   disable_on_destroy = true
+
+  depends_on [google_project_iam_binding.cloudbuild_sa_permissions]
 }
