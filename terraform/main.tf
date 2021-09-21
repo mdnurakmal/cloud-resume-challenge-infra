ab878623516e02572bddf63221820604c54c5599
mdnurakmal <mdnurakmal@gmail.com>
2021-09-21 22:46:36 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,5 +25,5 @@ locals {
   project = data.google_project.project.project_id
   project_number = data.google_project.project.number
   service_name   = "cloud-resume-challenge"
-  cloud_build_sa ="${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+  cloud_build_sa ="serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
