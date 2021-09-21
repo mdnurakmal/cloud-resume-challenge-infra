1fb73972a07d22c90660fc9cb01c27220319ae9c
mdnurakmal <mdnurakmal@gmail.com>
2021-09-22 01:52:26 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,8 +20,8 @@ if [[ $return_value == 0 ]]
 then
     echo "bucket exist"
     terraform -chdir=terraform init -backend-config="bucket=${1}-cloud-resume-challenge-bucket" -backend-config="prefix=terraform/state"
-    terraform -chdir=terraform state pull
-    terraform -chdir=terraform destroy -auto-approve -var project=${2} -var region=${_REGION}
+    #terraform -chdir=terraform state pull
+    #terraform -chdir=terraform destroy -auto-approve -var project=${2} -var region=${_REGION}
 
 
 else
