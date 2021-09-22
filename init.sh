7b56cb12cddc0b30c963b5dbdce7e0dbb98d0142
mdnurakmal <mdnurakmal@gmail.com>
2021-09-22 16:05:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Check if cloud resource manager is enabled
+
 
 isServiceEnabled=`gcloud services list --enabled | grep -w -c "cloudresourcemanager.googleapis.com"`
 
 if [[ $isServiceEnabled == 0 ]] 
 then
-    echo "cloudresourcemanager not enabled"
     gcloud services enable cloudresourcemanager.googleapis.com
 else
-    echo "cloudresourcemanager enabled"
 fi
 
 
@@ -16,15 +16,15 @@ gsutil -q stat gs://$1-cloud-resume-challenge-bucket/terraform/state/default.tfs
 
 return_value=$?
 
+# Check if bucket exists
+
 if [[ $return_value == 0 ]] 
 then
-    echo "bucket exist"
     terraform -chdir=terraform init -backend-config="bucket=${1}-cloud-resume-challenge-bucket" -backend-config="prefix=terraform/state"
     terraform -chdir=terraform state pull
     terraform -chdir=terraform destroy -auto-approve -var project=${2} -var region=${_REGION}
 
 
 else
-    echo "bucket doesnt exist"
     gsutil mb gs://$1-cloud-resume-challenge-bucket
 fi
